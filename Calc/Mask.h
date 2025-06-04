c312d720ce715a5f7256ea45a806a047ea93e8cf
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2025-06-04 22:11:33 +1000

    Prefix work

---
+++ Calc/Mask.h
@@ -0,0 +1,13 @@
+#include<Windows.h>
+#include<CommCtrl.h>
+#pragma once
+class Mask
+{
+	static const int nOctet = 4;
+	int octet[nOctet];
+	Mask(HWND)
+	{
+
+	}
+};
+
--- Calc/main.cpp
+++ Calc/main.cpp
@@ -4,6 +4,8 @@
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+int CountPrefixBits(DWORD mask);
+void SetMaskAndPrefix(HWND hIPmask, HWND hEditPrefix, DWORD dwMask);
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)\
 {
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG_MAIN), NULL, DlgProc, 0); // if DlgProc red, change x64 to x86  or add befor DlgProc (DLGPROC)
@@ -25,7 +27,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 	{
 		HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
-		HWND hIPmask = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+		HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
 		HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
 		DWORD dwIPaddress = 0;
 		DWORD dwIPmask = 0;
@@ -37,13 +39,26 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		case IDC_IPADDRESS:
 		{
 			SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
-			if (FIRST_IPADDRESS(dwIPaddress) < 128) SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFF000000);
-			else if (FIRST_IPADDRESS(dwIPaddress) < 192) SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFFFF0000);
-			else if (FIRST_IPADDRESS(dwIPaddress) < 224) SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFFFFFF00);
+			if (FIRST_IPADDRESS(dwIPaddress) < 128)  dwIPmask = 0xFF000000;
+			else if (FIRST_IPADDRESS(dwIPaddress) < 192)  dwIPmask = 0xFFFF0000;
+			else if (FIRST_IPADDRESS(dwIPaddress) < 224) dwIPmask = 0xFFFFFF00;
+			else dwIPmask = 0;
+			SetMaskAndPrefix(hIPmask, hEditPrefix, dwIPmask);
+		}
+		break;
+		case IDC_IPMASK:
+		{
+			if (dwIPmask != 0)
+			SendMessage(hEditPrefix, WM_SETTEXT, 0, CountPrefixBits(dwIPmask));
+		}
+		break;
+		case IDC_EDIT_PREFIX:
+		{
+			SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFFFFFFF0);
 		}
 		break;
 		case IDOK:
-			break;
+		break;
 		case IDCANCEL:
 			EndDialog(hwnd, 0);
 			break;
@@ -54,4 +69,33 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		EndDialog(hwnd, 0);
 	}
 	return FALSE;
+}
+int CountPrefixBits(DWORD mask)
+{    
+	if (mask != 0)
+	{
+
+	int count = 0;
+	for (int i = 31; i >= 0; --i) {
+		if ((mask >> i) & 1)
+			++count;
+		else
+			break; // stop first zero
+	}
+	return count;
+	}
+	return 0;
+}
+
+void SetMaskAndPrefix(HWND hIPmask, HWND hEditPrefix, DWORD dwMask)
+{
+	// Установить маску в IP-контрол
+	SendMessage(hIPmask, IPM_SETADDRESS, 0, dwMask);
+
+	// Сконвертировать префикс (число единичных бит) в строку
+	WCHAR szPrefix[4] = {};
+	wsprintf(szPrefix, L"%d", CountPrefixBits(dwMask));
+
+	// Установить текст в Edit-контрол
+	SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)szPrefix);
 }
