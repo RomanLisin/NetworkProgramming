aa29628b4c9b5c9848234ba9c0e1b66a249371cc
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2025-06-07 14:17:46 +1000

    Prefix Change

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -1,8 +1,12 @@
 #include<Windows.h>
 #include<CommCtrl.h>
 #include"resource.h"
+#define CHECK_BIT(var, pos) ((var) & (1<<(pos)))
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int MaskToPrefix(DWORD mask);
+DWORD PrefixToMask(int prefix);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)\
 {
@@ -14,15 +18,40 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-		{
 	case WM_INITDIALOG:
+	{
 		HWND hPrefix = GetDlgItem(hwnd, IDC_SPIN_PREFIX);
 		SendMessage(hPrefix, UDM_SETRANGE, 0, MAKELPARAM(32, 0));
 		//https://learn.microsoft.com/en-us/windows/win32/controls/udm-setrange
 		SetFocus(GetDlgItem(hwnd, IDC_IPADDRESS));
-		}
-		break;
+	}
+	break;
 
+	case WM_NOTIFY:
+	{
+		NMUPDOWN* pNMUpDown = (NMUPDOWN*)lParam;
+		if (pNMUpDown->hdr.idFrom == IDC_SPIN_PREFIX)
+		{
+			// get current value of prefix
+			int prefix = SendMessage(pNMUpDown->hdr.hwndFrom, UDM_GETPOS, 0, 0);
+			prefix += pNMUpDown->iDelta; // учитываем изменение
+
+			if (prefix < 0) prefix = 0;
+			if (prefix > 32) prefix = 32; 
+
+			DWORD mask = PrefixToMask(prefix);
+
+			// set new mask
+			HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
+			SendMessage(hIPmask, IPM_SETADDRESS, 0, mask);
+
+			// upgrade value in edit control
+			SetDlgItemInt(hwnd, IDC_EDIT_PREFIX, prefix, FALSE);
+
+			return TRUE;
+		}
+	}
+	break;
 		// For WM_COMMAND
 		// LOWORD(wParam) - who send message, HIWORD(wParam) - message code, what event happened to the sender
 		// LOWERD(lParam) - HWND, who sender, HIWORD(lParam) - don't use, = 0
@@ -37,13 +66,25 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		switch (LOWORD(wParam))
 		{
 		case IDC_IPADDRESS:
-		{
-			SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
-			if (FIRST_IPADDRESS(dwIPaddress) < 128) SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFF000000);
-			else if (FIRST_IPADDRESS(dwIPaddress) < 192) SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFFFF0000);
-			else if (FIRST_IPADDRESS(dwIPaddress) < 224) SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFFFFFF00);
-		}
+			if (HIWORD(wParam) == EN_CHANGE)
+			{
+				SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
+				if (FIRST_IPADDRESS(dwIPaddress) < 128) SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFF000000);
+				else if (FIRST_IPADDRESS(dwIPaddress) < 192) SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFFFF0000);
+				else if (FIRST_IPADDRESS(dwIPaddress) < 224) SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFFFFFF00);
+
+				SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+				int prefix = MaskToPrefix(dwIPmask);
+				SetDlgItemInt(hwnd, IDC_EDIT_PREFIX, prefix, FALSE);
+			}
 		break;
+		case IDC_IPMASK:
+			if(HIWORD(wParam) == EN_CHANGE)
+			{
+				SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+				int prefix = MaskToPrefix(dwIPmask);
+				SetDlgItemInt(hwnd, IDC_EDIT_PREFIX, prefix, FALSE);
+			}
 		case IDOK:
 			break;
 		case IDCANCEL:
@@ -56,4 +97,23 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		EndDialog(hwnd, 0);
 	}
 	return FALSE;
+}
+
+int MaskToPrefix(DWORD mask)
+{
+	int prefix = 0;
+	for (int i = 31; i >= 0; i--)
+	{
+		if (CHECK_BIT(mask,i))
+		{
+			prefix++;
+		}
+		else break;
+	}
+	return prefix;
+}
+
+DWORD PrefixToMask(int prefix)
+{
+	return (0xFFFFFFFF << (32 - prefix));
 }
